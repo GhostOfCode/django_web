f17dd58f6bb7050e6324510192ac4d0d3a39633d
Paladin <ghostofdeath2023@gmail.com>
2020-04-15 17:41:43 +0300

    Minoir fix

--- mysite/apps/articles/views.py
+++ mysite/apps/articles/views.py
@@ -4,9 +4,9 @@ from .models import Article
 from django.urls import reverse
 
 
-def index(requset):
+def index(requst):
     latest_articles_list = Article.objects.order_by('-pub_date')[:5]
-    return render(requset, 'articles/list.html', {'latest_articles_list': latest_articles_list})
+    return render(requst, 'articles/list.html', {'latest_articles_list': latest_articles_list})
 
 
 def detail(requst, article_id):
